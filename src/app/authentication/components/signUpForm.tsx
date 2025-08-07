33770e65d9ebe9341b5fd7f0b1d6a56e054b3e60
Thiago Luiz <thiago.luiz@convicti.com.br>
2025-08-06 23:15:20 -0300

    feat: add sign in with email and password

--- src/app/authentication/components/signUpForm.tsx
+++ src/app/authentication/components/signUpForm.tsx
@@ -69,8 +69,8 @@ const SignUpForm = () => {
         onError: (error) => {
           if (error.error.code === "USER_ALREADY_EXISTS") {
             toast.error("E-mail já cadastrado");
-            form.setError("email", {
-              message: error.error.message,
+            return form.setError("email", {
+              message: "E-mail já cadastrado.",
             });
           }
           toast.error(error.error.message);
